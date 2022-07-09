676bb4462e75d360a1ae7e9df357fb979a1d20cf
Dmitry <komuk11107@mail.ru>
2022-07-09 14:59:23 +0300

    Task6

---
+++ Task6 BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+class MyProgrman
+{
+    static void Main()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        int firsNum = 123;
+        object boxing = firsNum;
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+        sw.Restart();
+        sw.Start();
+        int secondNum = (int)boxing;
+        sw.Stop();
+        sw.Restart();
+        Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+    }
+}
