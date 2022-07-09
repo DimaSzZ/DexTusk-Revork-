84bcabf970bb79bf1793bf8757b93413b66df8af
Dmitry <komuk11107@mail.ru>
2022-07-09 15:17:26 +0300

    Task9

---
+++ Task 9 IComparable(Revork)/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_IComparable
+{
+    internal class Figure : IComparable<Figure>
+    {
+        private IComparable<Figure> _comparableImplementation;
+        public double Side { get; }
+        public double Square { get; set; }
+        public Figure(double side)
+        {
+            Side = side;
+            Square = Side * Side;
+        }
+        public int CompareTo(Figure? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            if (Square.CompareTo(other.Square) > 0) return -1;
+            if (Side.CompareTo(other.Side) > 0) return -1;
+            return 1;
+        }
+    }
+}
---
+++ Task 9 IComparable(Revork)/Program.cs
@@ -0,0 +1,18 @@
+﻿using Task_6_IComparable;
+class Program
+{
+    static void Main()
+    {
+        Figure[] figures = new Figure[10];
+        for (int i = 0; i < figures.Length; i++)
+        {
+            figures[i] = new Figure(new Random().Next(1, 50));
+        }
+        Array.Sort(figures);
+
+        foreach (Figure figure in figures)
+        {
+            Console.WriteLine($"Сторона:{figure.Side} - Площадь:{figure.Square}");
+        }
+    }
+}
